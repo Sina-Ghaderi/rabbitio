b682a3fa0c1faaae43fda9b8691dbaec6736e24c
Sina Ghaderi <32870524+Sina-Ghaderi@users.noreply.github.com>
2022-01-23 09:29:30 +0330

    Update iowrp.go

--- iowrp.go
+++ iowrp.go
@@ -6,7 +6,7 @@ import (
 )
 
 // NewWriterCipher warp a rabbit cipher stream with an io.Writer, returned StreamWriter
-// interface witch can be used to encrypt or decrypting data
+// interface which can be used to encrypt or decrypting data
 func NewWriterCipher(key []byte, iv []byte, wr io.Writer) (*cipher.StreamWriter, error) {
 	stream, err := NewCipher(key, iv)
 	if err != nil {
@@ -16,7 +16,7 @@ func NewWriterCipher(key []byte, iv []byte, wr io.Writer) (*cipher.StreamWriter,
 }
 
 // NewWriterCipher warp a rabbit cipher stream with an io.Reader, returned StreamReader
-// interface witch can be used to encrypt or decrypting data
+// interface which can be used to encrypt or decrypting data
 func NewReaderCipher(key []byte, iv []byte, re io.Reader) (*cipher.StreamReader, error) {
 	stream, err := NewCipher(key, iv)
 	if err != nil {
