df6a3df1d2cf144cca7a74a20d109957dad7c678
Sina Ghaderi <32870524+Sina-Ghaderi@users.noreply.github.com>
2022-07-30 19:07:34 +0430

    overlap unsafe

---
+++ overlap.go
@@ -0,0 +1,18 @@
+//go:build !purego
+
+package rabbitio
+
+import "unsafe"
+
+func AnyOverlap(x, y []byte) bool {
+	return len(x) > 0 && len(y) > 0 &&
+		uintptr(unsafe.Pointer(&x[0])) <= uintptr(unsafe.Pointer(&y[len(y)-1])) &&
+		uintptr(unsafe.Pointer(&y[0])) <= uintptr(unsafe.Pointer(&x[len(x)-1]))
+}
+
+func InexactOverlap(x, y []byte) bool {
+	if len(x) == 0 || len(y) == 0 || &x[0] == &y[0] {
+		return false
+	}
+	return AnyOverlap(x, y)
+}
