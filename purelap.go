d06dbf9e04a58306a99bdbfe27023734cfd72bc1
Sina Ghaderi <32870524+Sina-Ghaderi@users.noreply.github.com>
2022-07-30 19:08:11 +0430

    overlap reflect

---
+++ purelap.go
@@ -0,0 +1,18 @@
+//go:build purego
+
+package rabbitio
+
+import "reflect"
+
+func AnyOverlap(x, y []byte) bool {
+	return len(x) > 0 && len(y) > 0 &&
+		reflect.ValueOf(&x[0]).Pointer() <= reflect.ValueOf(&y[len(y)-1]).Pointer() &&
+		reflect.ValueOf(&y[0]).Pointer() <= reflect.ValueOf(&x[len(x)-1]).Pointer()
+}
+
+func InexactOverlap(x, y []byte) bool {
+	if len(x) == 0 || len(y) == 0 || &x[0] == &y[0] {
+		return false
+	}
+	return AnyOverlap(x, y)
+}
